e9267248f1176f301c4941fc9e411bca92f9fe25
Frank Benavides <fjos807@gmail.com>
2019-11-24 08:55:52 -0600

    Start of project

---
+++ Cliente/src/proyecto2/Proyecto2.java
@@ -0,0 +1,21 @@
+
+package proyecto2;
+
+import Interfaz.*;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author GRD
+ */
+public class Proyecto2 {
+    public static void main(String[] args) {
+        Inicio inicio = new Inicio();
+        inicio.setLocationRelativeTo(null);
+        inicio.setVisible(true);
+        inicio.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    }
+    
+}// Gerald x keilin(amor de mi vida)
